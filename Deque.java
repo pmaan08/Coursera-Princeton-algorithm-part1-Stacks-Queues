658fc82cc5fb411dc11bcfbf75bab86b0661ac7b
Partul Maan <41269832+pmaan08@users.noreply.github.com>
2020-05-07 23:19:03 +0530

    Create Deque.java

---
+++ Deque.java
@@ -0,0 +1,177 @@
+import java.util.*;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Deque<Item> implements Iterable<Item> 
+{
+ private Node first;
+ private Node last;
+   private int size;
+  
+  public Deque()
+{
+first=null;
+last=null;
+size=0;
+}
+private class Node
+{
+
+ private Item item;
+ private Node next;  
+ Node()
+ {
+	 item=null;
+	 next=null;
+ }
+ Node(Item x)
+ {
+  item=x;
+   next=null;
+}
+}
+
+// is the deque empty?
+ public boolean isEmpty()
+{
+  return size==0;
+}
+// return the number of items on the deque
+public int size1()
+{
+ return size;
+}
+
+
+
+// add the item to the front
+public void addFirst(Item item)
+{
+if(item==null)
+{
+throw new java.lang.NullPointerException();
+}
+Node ptr=first;
+ptr=first.next;
+first=new Node();
+first.item=item;
+first.next=ptr;
+if(size==0)
+{
+	last=first;
+}
+size++;
+}
+	 
+
+
+
+// add the item to the back
+public void addLast(Item item)
+{
+if(item==null)
+{
+throw new java.lang.NullPointerException();
+}
+Node ptr=last;
+last =new Node(item);
+ptr.next=last;
+size++;
+ }
+
+
+
+// remove and return the item from the front
+ public Item removeFirst()
+ {
+if (isEmpty()) 
+{throw new java.util.NoSuchElementException();
+}
+Item item=first.item;
+Node ptr=first;
+first=first.next;
+ptr.next=null;
+size--;
+return item;
+}
+
+
+// remove and return the item from the back
+public Item removeLast()
+{ 
+if (isEmpty()) 
+{ throw new java.util.NoSuchElementException();
+}
+Item item=last.item;
+Node ptr=first;
+while(ptr.next.next!=null)
+{ptr=ptr.next;
+}
+ptr.next=null;
+size--;
+return item;
+}
+
+// return an iterator over items in order from front to back
+public Iterator<Item> iterator()
+{
+return new ListIterator();
+}
+
+
+private class ListIterator implements Iterator<Item>
+ {
+private Node current=first;
+
+public boolean hasNext()
+{ return current!=null;
+}
+public void remove()
+{
+	throw new java.lang.UnsupportedOperationException();
+}
+public Item next()
+{
+Item item =current.item;
+ current=current.next;
+return item;
+}
+ } 
+
+//unit testing (required)
+ public static void main(String[] args) {
+        Deque<Integer> deque = new Deque<Integer>();
+        deque.addFirst(1);
+        for (Integer i : deque)
+            System.out.println(i);
+        System.out.println("-----");
+        deque.removeLast();
+       deque.addFirst(3);
+        deque.addFirst(2);
+        deque.addLast(9);
+        deque.addLast(10);
+        for (Integer i : deque)
+            System.out.println(i);
+        System.out.println("-----");
+
+        for (Integer i : deque)
+            for (Integer j : deque)
+                System.out.println(i + " " + j);
+        System.out.println("-----");
+        deque.removeFirst();
+        for (Integer i : deque)
+            System.out.println(i);
+        System.out.println("-----");
+        deque.removeLast();
+        for (Integer i : deque)
+                System.out.println(i);
+        System.out.println("-----");
+        System.out.println(deque.size);
+        deque.removeFirst();
+        deque.removeLast();
+        System.out.println(deque.size);
+    }
+}
+
+
+
